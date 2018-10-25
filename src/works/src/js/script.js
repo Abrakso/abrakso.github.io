4ca7bc45557269ad337eccd4ac6e43dd641ebbe8
Abrakso <44148694+Abrakso@users.noreply.github.com>
2018-10-25 10:11:58 +0800

    jsAdd

--- src/works/src/js/script.js
+++ src/works/src/js/script.js
@@ -4,8 +4,20 @@ $(document).ready(function () {
         $('.logo__button').toggleClass('logo__button_active');
     });
 
-    $('.menu__item').on("click", function () {
+    $('.menu__item').on("click", function (event) {
         $('.menu').removeClass('menu_fullscreen');
         $('.logo__button').removeClass('logo__button_active');
+
+        event.preventDefault();
+        var id  = $(this).attr('href');
+        var top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
     });
+
+    // $('.menu__item').on("click", function (event) {
+    //     event.preventDefault();
+    //     var id  = $(this).attr('href');
+    //     var top = $(id).offset().top;
+    //     $('body,html').animate({scrollTop: top}, 1500);
+    // });
 });
